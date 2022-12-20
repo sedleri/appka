e29dc4aa67b2aaf69e6344d48597de0eb92f0e1e
@sedleri <richard.sedlecky@seznam.cz>
2022-12-20 21:35:47 +0000

    jebe

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header class="flex justify-between">
-	<nav>
+	<nav class="flex justify-between bg-white">
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
@@ -22,12 +22,6 @@
 </header>
 
 <style> 
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
 	svg {
 		width: 2em;
 		height: 3em;
